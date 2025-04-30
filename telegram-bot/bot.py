12b15d765779d4b07dc622531d1867fb5d1d13eb
Dden59 <prmden90@gmail.com>
2025-04-30 13:42:57 +0300

    Create bot.py

---
+++ telegram-bot/bot.py
@@ -0,0 +1,28 @@
+import os
+import telebot
+from telebot.types import WebAppInfo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv('7921478924:AAEveJLy-VD0JXC---Aq-Bj9kqOOCqJy8X0'))
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = telebot.types.KeyboardButton(
+        text="🚀 Открыть Rocket Queen Pro",
+        web_app=WebAppInfo(url="https://your-github-pages.url")
+    )
+    markup.add(btn)
+    
+    bot.send_message(
+        message.chat.id,
+        "Добро пожаловать в *Rocket Queen Pro*!\n\n"
+        "Нажмите кнопку ниже для запуска помощника",
+        reply_markup=markup,
+        parse_mode="Markdown"
+    )
+
+if __name__ == '__main__':
+    bot.polling()
